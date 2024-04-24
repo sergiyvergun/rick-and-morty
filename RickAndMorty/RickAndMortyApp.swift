ed6164b697a45dac2e61bb95b924dfc36fb9589a
Sergiy <sergiyvergun01@gmail.com>
2024-04-25 01:11:37 +0200

    Initial Commit

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Sergiy Vergun on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
